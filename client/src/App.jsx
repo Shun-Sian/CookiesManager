e1b54e80c69d81810daa399ac93689b6663e5b2e
Shun-Sian <qvor_dragomirov@abv.bg>
2025-02-12 00:23:53 +0200

    cleanup part 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,6 @@ import AdminLogin from './Admin/AdminLogin';
 import CookieConsent from './Banner/CookieConsent';
 
 function App() {
-  // const [subsections, setSubsections] = useState([]);
-
-  // useEffect(() => {
-  //   // Load subsections from localStorage, if available
-  //   const storedSubsections = JSON.parse(localStorage.getItem('subsections'));
-  //   if (storedSubsections) {
-  //     setSubsections(storedSubsections);
-  //   }
-  // }, []);
-
-  // const handleUpdateSubsections = (newSubsections) => {
-  //   setSubsections(newSubsections);
-  //   localStorage.setItem('subsections', JSON.stringify(newSubsections)); // Save subsections to localStorage
-  // };
   return (
     <BrowserRouter>
       <div className="App">
